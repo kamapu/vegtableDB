1d125f54040f8996da07acfa82c215fb72b6e3bd
Miguel Alvarez <kamapu78@gmail.com>
2022-01-09 12:49:38 +0100

    Added function "get_description()"

--- data-raw/create-data.R
+++ data-raw/create-data.R
@@ -1,5 +1,5 @@
 # TODO:   Script creating data from data-raw
-# 
+#
 # Author: Miguel Alvarez
 ################################################################################
 
@@ -9,8 +9,9 @@ library(readODS)
 
 db_catalog <- list()
 
-for (i in c("taxonomy", "databases", "relations"))
+for (i in c("taxonomy", "databases", "relations")) {
   db_catalog[[i]] <- read_ods("data-raw/db_catalog.ods", i)
+}
 
 # Final: Export data
 save(db_catalog, file = "R/sysdata.rda")
---
+++ lab/04-test-catalog.R
@@ -0,0 +1,13 @@
+# TODO:   Test the use of catalog for import session
+# 
+# Author: Miguel Alvarez
+################################################################################
+
+library(RPostgreSQL)
+library(dbaccess)
+
+DB <- "veg_databases"
+conn <- connect_db2(DB, user = "miguel")
+
+Test
+
