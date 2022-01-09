4d8bc841642b5bf4d71393bf14ebb472cda2fbb3
Miguel Alvarez <kamapu78@gmail.com>
2022-01-09 11:02:19 +0100

    Database catalog exported

---
+++ data-raw/create-data.R
@@ -0,0 +1,16 @@
+# TODO:   Script creating data from data-raw
+# 
+# Author: Miguel Alvarez
+################################################################################
+
+# 1: Importing database catalogue
+
+library(readODS)
+
+db_catalog <- list()
+
+for (i in c("taxonomy", "databases", "relations"))
+  db_catalog[[i]] <- read_ods("data-raw/db_catalog.ods", i)
+
+# Final: Export data
+save(db_catalog, file = "R/sysdata.rda")
